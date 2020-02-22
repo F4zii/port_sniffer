407e4236b3d12a0a90ec65899ed9f36da2bda811
F4zii <fazbosoreq@gmail.com>
2020-02-22 23:47:35 +0200

    import VALID_FLAGS constant

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
-pub mod output_handler {
+use crate::constants::VALID_FLAGS;
 
+pub mod output_handler {
     pub fn print_data(output: Vec<u16>) {
         println!(); // separate the output
         for v in output {
@@ -8,4 +9,12 @@ pub mod output_handler {
     }
 }
 
+
+pub mod input_handler {
+    use crate::constants::VALID_FLAGS;
+
+    pub fn is_flag_valid(flag: &str) -> bool {
+        return VALID_FLAGS.iter().any(|s| *s == flag)
+    }
+}
 
